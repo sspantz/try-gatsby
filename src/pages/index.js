877cb96d4b6dfaa4145c81f6ab27e5ce68205496
sspantz <sspantiezheng@gmail.com>
2019-12-16 15:31:02 +0800

    improvement(index.js): add some contents

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div
+    style={{
+      color: `puple`,
+    }}
+  >
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x300" alt="400x300" />
+  </div>
+)
